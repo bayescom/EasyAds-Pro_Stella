7c7eca30c350fd3104f2a9582865c6c277b02b56
luyao <lightyear5235@gmail.com>
2026-09-21 18:52:03 +0800

    update

--- lua/tools/conf_cache.lua
+++ lua/tools/conf_cache.lua
@@ -29,7 +29,6 @@ local supplier_conf = {
     cache = redis_utils.createLruCache(MEDIUM_CACHE_SIZE),
     is_zip = false,
     check_empty = true,
-    md5kv_key = 'MD5KV',
 }
 
 -- custom adn conf，与 Luna SdkCustomAdnConfExportTask 对应，独立 db
@@ -41,7 +40,6 @@ local custom_adn_conf = {
     cache = redis_utils.createLruCache(MEDIUM_CACHE_SIZE),
     is_zip = false,
     check_empty = false,
-    md5kv_key = 'MD5KV',
 }
 
 local function redisArrayToTable(redis_array)
@@ -53,24 +51,16 @@ local function redisArrayToTable(redis_array)
     return redis_tbl
 end
 
-local function getConfCacheKey(one_conf, key)
-    if utils.isEmpty(one_conf.key_prefix) then
-        return key
-    end
-    return one_conf.key_prefix .. ':' .. key
-end
-
-local function getMd5ConfHashMap(redis_client, md5kv_key)
-    md5kv_key = md5kv_key or 'MD5KV'
+local function getMd5ConfHashMap(redis_client)
     -- 获取所有的md5
     local status, res = pcall(
         function ()
-            return redis_client:hgetall(md5kv_key)
+            return redis_client:hgetall('MD5KV')
         end
     )
 
     if not status then
-        ngx.log(ngx.ERR, 'get ' .. md5kv_key .. ' failed' .. res)
+        ngx.log(ngx.ERR, 'get MD5KV failed' .. res)
         return nil
     end
 
@@ -90,12 +80,12 @@ function _M.conf_cache_base(one_conf)
     -- 获取所有的md5
     local status, md5_res = pcall(
         function ()
-            return getMd5ConfHashMap(one_conf.redis_client, one_conf.md5kv_key)
+            return getMd5ConfHashMap(one_conf.redis_client)
         end
     )
 
     if not status then
-        ngx.log(ngx.ERR, 'get ' .. (one_conf.md5kv_key or 'MD5KV') .. ' failed for ' .. one_conf.name)
+        ngx.log(ngx.ERR, 'get MD5KV failed for ' .. one_conf.name)
         return nil
     end
 
@@ -194,7 +184,6 @@ local function getConfById(key, conf)
     local conf_cache = conf.cache
     local conf_redis_client = conf.redis_client
     local is_zip = conf.is_zip
-    key = getConfCacheKey(conf, key)
 
     local conf_tbl = conf_cache:get(key)
     -- 读取redis的中新的配置并塞入cache的行为在这里进行，需要读取update_tag来判断是否需要更新
